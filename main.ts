4fe71f40eb84ba14e00b87b580c48a6b0c2509f3
djr55slrd <95696083+djr55slrd@users.noreply.github.com>
2024-03-19 15:46:58 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     if (Switch) {
         Radio += -1
+        if (Radio < 1) {
+            Radio = RadioMax
+        }
+    }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    if (Switch) {
+        Radio += 1
+        if (Radio > RadioMax) {
+            Radio = 1
+        }
     }
 })
 let Y = 0
 let XX = 0
 let X = 0
 let Switch = 0
+let Radio = 0
+let RadioMax = 0
 joystickbit.initJoystickBit()
-let RadioMax = 6
-let Radio = 1
+RadioMax = 6
+Radio = 1
 radio.setGroup(Radio)
 Switch = 0
 basic.forever(function () {
@@ -28,5 +41,12 @@ basic.forever(function () {
     if (XX != X) {
         XX = X
         radio.sendValue("Y", XX)
+    }
+    if (Switch) {
+        radio.setGroup(Radio)
+        basic.showNumber(Radio)
+        basic.pause(2000)
+        basic.clearScreen()
+        Switch = 0
     }
 })
