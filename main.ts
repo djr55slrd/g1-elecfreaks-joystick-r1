2bd340cdc1b325f37221159c7b3eba87484c85dc
djr55slrd <95696083+djr55slrd@users.noreply.github.com>
2024-03-20 11:14:53 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+bitcommander.onEvent(BCPins.Green, BCEvents.Up, function () {
+    radio.sendValue("S", 1)
+})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendValue("A", 1)
+})
+bitcommander.onEvent(BCPins.Yellow, BCEvents.Down, function () {
+    radio.sendValue("W", 0)
+})
+bitcommander.onEvent(BCPins.Joystick, BCEvents.Up, function () {
+    radio.sendValue("Z", 1)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendValue("AB", 1)
@@ -8,6 +17,12 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUT
     RadioSwtch = 1
     RadioDisplay = RadioDly
     basic.showNumber(RadioGroup)
+})
+bitcommander.onEvent(BCPins.Blue, BCEvents.Down, function () {
+    radio.sendValue("E", 0)
+})
+bitcommander.onEvent(BCPins.Green, BCEvents.Down, function () {
+    radio.sendValue("S", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     if (RadioSwtch != 0) {
@@ -24,6 +39,9 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTT
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendValue("B", 1)
 })
+bitcommander.onEvent(BCPins.Yellow, BCEvents.Up, function () {
+    radio.sendValue("W", 1)
+})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     if (RadioSwtch != 0) {
         RadioGroup += 1
@@ -36,6 +54,18 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTT
         radio.sendValue("B", 0)
     }
 })
+bitcommander.onEvent(BCPins.Joystick, BCEvents.Down, function () {
+    radio.sendValue("Z", 0)
+})
+bitcommander.onEvent(BCPins.Red, BCEvents.Down, function () {
+    radio.sendValue("N", 0)
+})
+bitcommander.onEvent(BCPins.Red, BCEvents.Up, function () {
+    radio.sendValue("N", 1)
+})
+bitcommander.onEvent(BCPins.Blue, BCEvents.Up, function () {
+    radio.sendValue("E", 1)
+})
 let YWas = 0
 let Y = 0
 let XWas = 0
@@ -45,7 +75,7 @@ let RadioDly = 0
 let RadioSwtch = 0
 let RadioMax = 0
 let RadioGroup = 0
-RadioGroup = 2
+RadioGroup = 4
 RadioMax = 6
 RadioSwtch = 1
 RadioDly = 10
@@ -53,7 +83,7 @@ RadioDisplay = RadioDly
 radio.setGroup(RadioGroup)
 basic.showNumber(RadioGroup)
 basic.forever(function () {
-    X = 0
+    X = bitcommander.readJoystick(BCJoystick.X)
     if (X > 448 && X < 576) {
         X = 512
     }
@@ -61,7 +91,7 @@ basic.forever(function () {
         radio.sendValue("X", X)
         XWas = X
     }
-    Y = 0
+    Y = bitcommander.readJoystick(BCJoystick.Y)
     if (Y > 448 && Y < 576) {
         Y = 512
     }
