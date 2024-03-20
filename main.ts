3ae9d7e36027685a4379715df14bfc861425ba8a
djr55slrd <95696083+djr55slrd@users.noreply.github.com>
2024-03-20 13:39:12 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("A", 0)
-})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendValue("A", 1)
 })
@@ -11,24 +8,6 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUT
     RadioSwtch = 1
     RadioDisplay = RadioDly
     basic.showNumber(RadioGroup)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("B", 0)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("E", 1)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("S", 1)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("S", 0)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("W", 1)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("N", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     if (RadioSwtch != 0) {
@@ -41,9 +20,6 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTT
     } else {
         radio.sendValue("A", 0)
     }
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("B", 1)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendValue("B", 1)
@@ -60,18 +36,11 @@ control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTT
         radio.sendValue("B", 0)
     }
 })
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("N", 1)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
-    radio.sendValue("A", 1)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("E", 0)
-})
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendValue("W", 0)
-})
+let P16 = 0
+let P15 = 0
+let P14 = 0
+let P13 = 0
+let P8 = 0
 let YWas = 0
 let Y = 0
 let XWas = 0
@@ -81,15 +50,15 @@ let RadioDly = 0
 let RadioSwtch = 0
 let RadioMax = 0
 let RadioGroup = 0
-RadioGroup = 3
+RadioGroup = 5
 RadioMax = 6
 RadioSwtch = 1
-RadioDly = 10
+RadioDly = 15
 RadioDisplay = RadioDly
 radio.setGroup(RadioGroup)
 basic.showNumber(RadioGroup)
 basic.forever(function () {
-    X = 0
+    X = pins.analogReadPin(AnalogPin.P1)
     if (X > 448 && X < 576) {
         X = 512
     }
@@ -97,13 +66,29 @@ basic.forever(function () {
         radio.sendValue("X", X)
         XWas = X
     }
-    Y = 0
+    Y = pins.analogReadPin(AnalogPin.P2)
     if (Y > 448 && Y < 576) {
         Y = 512
     }
     if (YWas != Y) {
         radio.sendValue("Y", Y)
         YWas = Y
+    }
+    if (P8 != pins.digitalReadPin(DigitalPin.P8)) {
+        P8 = pins.digitalReadPin(DigitalPin.P8)
+        radio.sendValue("Z", P8)
+    } else if (P13 != pins.digitalReadPin(DigitalPin.P13)) {
+        P13 = pins.digitalReadPin(DigitalPin.P13)
+        radio.sendValue("S", P13)
+    } else if (P14 != pins.digitalReadPin(DigitalPin.P14)) {
+        P14 = pins.digitalReadPin(DigitalPin.P14)
+        radio.sendValue("E", P14)
+    } else if (P15 != pins.digitalReadPin(DigitalPin.P15)) {
+        P15 = pins.digitalReadPin(DigitalPin.P15)
+        radio.sendValue("N", P15)
+    } else if (P16 != pins.digitalReadPin(DigitalPin.P16)) {
+        P16 = pins.digitalReadPin(DigitalPin.P16)
+        radio.sendValue("W", P16)
     }
     if (RadioDisplay > 0) {
         RadioDisplay += -1
