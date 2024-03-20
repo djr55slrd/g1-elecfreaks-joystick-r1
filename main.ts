fafff38222cb6f5bfb2ee7063b334f7e8b316435
djr55slrd <95696083+djr55slrd@users.noreply.github.com>
2024-03-20 09:31:45 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,82 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendValue("A", 1)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendValue("AB", 1)
+})
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    Switch = 1
+    RadioSwtch = 1
+    RadioDisplay = RadioDly
+    basic.showNumber(RadioGroup)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    if (Switch) {
-        Radio += -1
-        if (Radio < 1) {
-            Radio = RadioMax
+    if (RadioSwtch != 0) {
+        RadioGroup += -1
+        if (RadioGroup < 1) {
+            RadioGroup = RadioMax
         }
+        RadioDisplay = RadioDly
+        basic.showNumber(RadioGroup)
+    } else {
+        radio.sendValue("A", 0)
     }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendValue("B", 1)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    if (Switch) {
-        Radio += 1
-        if (Radio > RadioMax) {
-            Radio = 1
+    if (RadioSwtch != 0) {
+        RadioGroup += 1
+        if (RadioGroup > RadioMax) {
+            RadioGroup = 1
         }
-        Switch = 1
-        basic.showNumber(Radio)
+        RadioDisplay = RadioDly
+        basic.showNumber(RadioGroup)
+    } else {
+        radio.sendValue("B", 0)
     }
 })
+let YWas = 0
 let Y = 0
-let XX = 0
+let XWas = 0
 let X = 0
-let Switch = 0
-let Radio = 0
+let RadioDisplay = 0
+let RadioDly = 0
+let RadioSwtch = 0
 let RadioMax = 0
+let RadioGroup = 0
 joystickbit.initJoystickBit()
+RadioGroup = 1
 RadioMax = 6
-Radio = 1
-radio.setGroup(Radio)
-Switch = 0
+RadioSwtch = 1
+RadioDly = 10
+RadioDisplay = RadioDly
+radio.setGroup(RadioGroup)
+basic.showNumber(RadioGroup)
 basic.forever(function () {
-    X = joystickbit.getRockerValue(joystickbit.rockerType.X)
+    X = 1023 - joystickbit.getRockerValue(joystickbit.rockerType.X)
     if (X > 448 && X < 576) {
         X = 512
     }
-    if (XX != X) {
-        XX = X
-        radio.sendValue("X", XX)
+    if (XWas != X) {
+        radio.sendValue("X", X)
+        XWas = X
     }
     Y = joystickbit.getRockerValue(joystickbit.rockerType.Y)
     if (Y > 448 && Y < 576) {
         Y = 512
     }
-    if (XX != X) {
-        XX = X
-        radio.sendValue("Y", XX)
+    if (YWas != Y) {
+        radio.sendValue("Y", Y)
+        YWas = Y
     }
-    if (Switch) {
-        radio.setGroup(Radio)
-        basic.showNumber(Radio)
-        basic.pause(2000)
-        basic.clearScreen()
-        Switch = 0
+    if (RadioDisplay > 0) {
+        RadioDisplay += -1
+        basic.pause(100)
+        if (RadioDisplay == 0) {
+            RadioSwtch = 0
+            radio.setGroup(RadioGroup)
+            basic.clearScreen()
+        }
     }
 })
